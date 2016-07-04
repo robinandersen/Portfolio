17bef20980e49140491bc9399381b242c1ca2837
Robin Andersen <studio@robinandersen.no>
2016-07-04 17:51:40 +0200

    First Commit

---
+++ site/snippets/footer.php
@@ -0,0 +1,27 @@
+  	<footer>
+
+  		<!--<p>Copyright © Robin Andersen <?php echo date("Y") ?></p>
+
+  		<ul>
+  			<li><a href="#">Behance</a></li>
+  			<li><a href="#">Dribbble</a></li>
+  			<li><a href="#">Twitter</a></li>
+  		</ul>-->
+
+  	</footer>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-70355006-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+
+
+</html>
