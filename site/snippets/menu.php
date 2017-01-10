847c4d3f0e9e73542a0f6aa4e038524abd451341
Robin Andersen <studio@robinandersen.no>
2017-01-10 21:15:56 +0100

    Revert "First Commit"
    This reverts commit 17bef20980e49140491bc9399381b242c1ca2837.
    
    # Conflicts:
    #	assets/css/main.css
    #	assets/css/main.css.map
    #	assets/css/main.sass
    #	content/2-about/about.txt
    #	site/accounts/robin.php
    #	site/blueprints/about.php
    #	site/templates/about.php

--- site/snippets/menu.php
+++
@@ -1,9 +0,0 @@
-<nav role="navigation">
-  <ul>
-    <?php foreach($pages->visible() as $p): ?>
-    <li>
-      <a <?php e($p->isOpen(), 'class="active"') ?> href="<?php echo $p->url() ?>"><?php echo $p->title()->html() ?></a>
-    </li>
-    <?php endforeach ?>
-  </ul>
-</nav>
